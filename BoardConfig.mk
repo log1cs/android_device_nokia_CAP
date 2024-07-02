595b6793952ef04479e03fdcb17aa0a2ccff06e6
jmpfbmx <jmpf.bmx@gmail.com>
2024-07-02 10:24:40 +0700

    CAP: Add arch, bootloader and platform flags
    Change-Id: I68196f2a2a0bf06596c741741296ed3a8c47c223

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,5 +4,31 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_PATH := device/nokia/CAP
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := trinket
+TARGET_NO_BOOTLOADER := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a73
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a73
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := trinket
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno610
+
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_CAP_defconfig

6623e905082b75760e492ce2f7f4643ed2f2c9a0
jmpfbmx <jmpf.bmx@gmail.com>
2024-07-02 10:37:51 +0700

    CAP: Set up partitions, A/B configurations
    Change-Id: Ifebb6b2e76fad380a62f79ff3559c9a901b859df

---
+++ device.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2014 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=erofs \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script

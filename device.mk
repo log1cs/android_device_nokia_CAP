838c8713dd1d3e67c244b1270049dff2bdcb2f32
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-07-06 01:31:58 +0700

    CAP: Build fastbootd
    Change-Id: Ifa2eb23848e7ef54be6aec3c9d80cdca057c91c8

--- device.mk
+++ device.mk
@@ -140,6 +140,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.4-service.clearkey
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor

f67ba396a6b674b2c59112bf93f06834e4873ce0
jmpfbmx <jmpf.bmx@gmail.com>
2024-07-06 00:53:23 +0700

    CAP: Build IPACM
    Change-Id: Ia179f7be4d5592932394f40c2d2be20d4a0f049d
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- device.mk
+++ device.mk
@@ -181,6 +181,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-hotword.xml
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    libipanat \
+    liboffloadhal
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl

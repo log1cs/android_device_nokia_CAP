93c177d41123245934fbca296575fb02b3ba2bc2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-07-06 01:54:12 +0700

    CAP: Build SDM LiveDisplay HIDL
    Change-Id: I5f9b1167b5fd92dd0d2ab5711b26cdc1c519a29c

--- device.mk
+++ device.mk
@@ -211,6 +211,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \

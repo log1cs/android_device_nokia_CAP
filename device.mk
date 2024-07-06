33682e8af91d8f8f181942b39d79e749d447c028
GPUCode <geoster3d@gmail.com>
2024-07-06 10:52:08 +0700

    CAP: Import qti_whitelist permissions
    Change-Id: I18908c4ded4b57eca9244f2dc8e17f9a624534a4

--- device.mk
+++ device.mk
@@ -231,6 +231,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
+# Low-Power whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \

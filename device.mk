7faba8860d7bb67edbb1f3618f8ca681cb8da34d
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2024-07-22 21:33:21 +0700

    CAP: Build QTI health implementation
    Add QTI health implementation so that it waits for battery power
    supply for a max timeout of 5 seconds. This is to ensure by the
    time health HAL service starts, battery power supply device is
    available. Without this, health HAL couldn't find battery power
    supply sometimes because of the time taken for battery charger
    driver to probe on platforms where it can happen only after the
    remote subsytem is bought out of reset.
    
    Change-Id: I96ee53ef15dd61c98d317e9ec221456ca564249f

--- device.mk
+++ device.mk
@@ -203,8 +203,9 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-service \
-    android.hardware.health@2.1-impl
+    android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-impl-qti.recovery \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \

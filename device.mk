a2ef81ba728ffd83f92940534d1c81f6646875cd
Tuan Anh <tuan73176@gmail.com>
2024-07-29 23:27:29 +0700

    CAP: Cleanup camera packages
    Change-Id: I08fcb540e4d8799877b9c27149ee419bb1cb49f2

--- device.mk
+++ device.mk
@@ -108,12 +108,13 @@ TARGET_SCREEN_WIDTH := 720
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    libcamera2ndk_vendor \
-    libgui_vendor \
-    libpng.vendor
+    android.hardware.camera.provider@2.4-impl:32 \
+    android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0.vendor
+
+PRODUCT_PACKAGES += \
+    libcamera_metadata.vendor:32 \
+    libpng.vendor:32
 
 # Configstore
 PRODUCT_PACKAGES += \

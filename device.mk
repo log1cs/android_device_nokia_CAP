9494bb0c29375acfafaff685535c7e6dccd15aa3
Cosmin Tanislav <demonsingur@gmail.com>
2025-04-14 18:59:34 +0700

    CAP: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25

--- device.mk
+++ device.mk
@@ -84,9 +84,8 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 # Boot animation resolution
 TARGET_SCREEN_HEIGHT := 1600

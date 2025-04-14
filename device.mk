d0cc0cfa94fe89e9f636addf53c2b3aec76695e2
Michael Bestas <mkbestas@lineageos.org>
2025-04-14 19:01:09 +0700

    CAP: Don't use BSG framework for gpt-utils
    Change-Id: Ia6e5d651560c8ae5186a48bfbb0e30b864150564

--- device.mk
+++ device.mk
@@ -87,6 +87,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 # Boot animation resolution
 TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 720

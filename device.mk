25698db4131fa0ac9fe19ca95ac05d3af5c0c663
Luofan Chen <clfbbn@gmail.com>
2025-04-14 17:58:42 +0700

    CAP: Move to common Lineage fingerprint AIDL
    Change-Id: I8bb34a7856846a5e34466b838ab24b6bd53ca623

--- device.mk
+++ device.mk
@@ -163,7 +163,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint-service.lineage
 
 # FM
 PRODUCT_PACKAGES += \

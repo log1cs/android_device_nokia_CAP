d3c941e2cd7c89b5b7ad73423a28da12a4d31290
Tuan Anh <tuan73176@gmail.com>
2025-04-14 19:32:59 +0700

    Revert "CAP: Switch from time_daemon to TimeKeep"
    It's not really broken.
    
    This reverts commit 6473dba3a25fc726cffff1d1708d6928da76946a.
    
    Change-Id: I86404b88e11691cb5f2c40adfde3378e8253db7f

--- device.mk
+++ device.mk
@@ -382,10 +382,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
-# TimeKeep
-PRODUCT_PACKAGES += \
-    TimeKeep
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.qti \

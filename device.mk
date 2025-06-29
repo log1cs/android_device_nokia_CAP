208d98e025075e5a3f8164e15820d5e1eec81061
basamaryan <basam.aryan@gmail.com>
2025-06-29 16:22:30 +0700

    CAP: media: Import Codec2 configs
    From sunfish TQ3A.230805.001.S1
    
    Change-Id: Ie2ecfa912e0fe0892a2e6b8ddc3a6b6aa3213d40

--- device.mk
+++ device.mk
@@ -193,6 +193,8 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
 # Neural Networks

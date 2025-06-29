b93ca56d01a2f6e8518db777481258872ed28aba
Arian <arian.kulmer@web.de>
2025-06-29 16:38:24 +0700

    CAP: init: Remove unnecessary class_core script
    Change-Id: I065407a7b9aaf186a22964786af97b36a140c793

--- device.mk
+++ device.mk
@@ -295,7 +295,6 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \
-    init.qcom.class_core.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh \

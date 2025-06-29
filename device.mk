26878a00076bf3020895142b18f3a6c48fd0bada
danascape <danascape@gmail.com>
2025-06-29 22:44:10 +0700

    CAP: Drop atrace HAL
    It is deprecated in Android 14.
    
    Change-Id: I5af8056853058ad29be7debb5c5af5dea83d84a9

--- device.mk
+++ device.mk
@@ -33,10 +33,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \

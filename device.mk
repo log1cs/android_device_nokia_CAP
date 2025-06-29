68a0bd9467b4dd27bf6d7b1295d5ddf5ab450304
Yumi Yukimura <me.cafebabe@gmail.com>
2025-06-29 16:37:28 +0700

    CAP: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- device.mk
+++ device.mk
@@ -107,7 +107,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 PRODUCT_PACKAGES += \
     gralloc.qcom \
     hwcomposer.qcom \
-    memtrack.trinket \
     libvulkan \
     libqdMetaData \
     libsdmcore
@@ -116,8 +115,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.composer@2.4-service \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    vendor.qti.hardware.memtrack-service \
     vendor.qti.hardware.display.allocator-service \
 
 # DRM
